6642251886dc66e5d20444283c012249e9897cd3
bolderstephanie <bolderstephanie@gmail.com>
2020-05-11 22:06:05 +0200

    Agenda pagina gemaakt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        roze: '#FF5B5E' ,
+        grijs: '#F7F5F4' ,
+        bookedgroen: '#8ED650' ,
+        waitingoranje: '#F6A62B' ,
+      }
+    },
   },
   variants: {},
   plugins: [],
