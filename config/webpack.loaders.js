943b6a1e9d984575d48fecc54f26347ac74683eb
bolderstephanie <bolderstephanie@gmail.com>
2020-06-11 16:01:40 +0200

    React toegevoegd

--- config/webpack.loaders.js
+++ config/webpack.loaders.js
@@ -25,7 +25,7 @@ const js = {
     {
       loader: "babel-loader",
       options: {
-        presets: ["@babel/preset-env"],
+        presets: ["@babel/preset-env", "@babel/preset-react"],
       },
     },
     //"eslint-loader",
---
+++ src/javascripts/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import yogaJson from './yoga.json';
+const App = () => {
+    const [poses, setPoses] = useState([]);
+    const [score, setScore] = useState(0);
+  
+    const onCompleted = (index) => {
+        setPoses(
+            poses.map((item,ind) => 
+                ind == index 
+                ? {...item, completed : true} 
+                : item 
+        ))
+        setScore(prevScore => prevScore + 1);
+        
+    }
+
+    useEffect(() => {
+        setPoses(yogaJson.data);
+    },[])
+
+    return (
+        <div>
+            {
+                poses.map(({pose, completed, score, imageUrl},index) => {
+                    return <YogaPose onCompleted={() => onCompleted(index)} pose={pose} completed={completed} score={score} imageUrl={imageUrl}/>
+                })
+            }
+            <p>{score}</p>
+        </div>
+    )
+}
+
+
+
+const YogaPose = ({pose, onCompleted, completed, score, imageUrl}) => {
+    return (
+    <div class="shadow-xl px-2 mx-6 mt-2 py-2 bg-white flex justify-between items-center">
+    <div>
+      <img class="items-center pl-2 h-12 w-14" src={imageUrl}/>
+    </div>
+    <div class=''>
+        <div class="text-black">{pose}</div>
+      </div>
+      <div class='flex items-center text-right'>
+        <div class=''>
+<button onClick={() => onCompleted()}class="bg-bookedgroen rounded text-lg text-center text-white py-1 px-2">{completed ? "completed" : `+${score}`}</button>
+        </div>
+      </div>
+    
+    </div>
+    )
+}
+
+export default App;
